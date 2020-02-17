37ba0f542abdf8ff460791b18e3e117e444cb334
Amendoza20 <20.angel.mendoza@gmail.com>
2020-02-17 14:01:36 -0500

    added repository

---
+++ src/main/java/com/UrbanTea/whiteAndFern/Model/galleryItem.java
@@ -0,0 +1,4 @@
+package com.UrbanTea.whiteAndFern.Model;
+
+public class galleryItem {
+}
---
+++ src/main/java/com/UrbanTea/whiteAndFern/Repository/workshopRepository.java
@@ -0,0 +1,6 @@
+package com.UrbanTea.whiteAndFern.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface workshopRepository extends JpaRepository {
+}
