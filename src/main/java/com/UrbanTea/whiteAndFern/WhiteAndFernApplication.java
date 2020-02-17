714e9274853f7929f3022b2bfe57f468a5b717aa
Amendoza20 <20.angel.mendoza@gmail.com>
2020-02-17 13:30:21 -0500

    Setting up back end config

---
+++ src/main/java/com/UrbanTea/whiteAndFern/WhiteAndFernApplication.java
@@ -0,0 +1,13 @@
+package com.UrbanTea.whiteAndFern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteAndFernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteAndFernApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UrbanTea/whiteAndFern/WhiteAndFernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UrbanTea.whiteAndFern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteAndFernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
