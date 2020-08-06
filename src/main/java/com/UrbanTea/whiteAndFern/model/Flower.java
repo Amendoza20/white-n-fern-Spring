ae6e91961f8f99d9531a3be368d585d403c1abee
Amendoza20 <20.angel.mendoza@gmail.com>
2020-08-05 22:20:03 -0400

    added names to flowers

--- src/main/java/com/UrbanTea/whiteAndFern/model/Flower.java
+++ src/main/java/com/UrbanTea/whiteAndFern/model/Flower.java
@@ -13,14 +13,17 @@ public class Flower {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String imagePath;
+    private String name;
 
-    public Flower(long id, String imagePath) {
+    public Flower (long id, String imagePath, String name) {
         this.id = id;
         this.imagePath = imagePath;
+        this.name = name;
     }
 
-    public Flower(String imagePath) {
+    public Flower (String imagePath, String name) {
         this.imagePath = imagePath;
+        this.name = name;
     }
 
     public long getId() {
@@ -34,4 +37,8 @@ public class Flower {
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
+
+    public String getName() {return name;}
+
+    public void setName(String name) { this.name = name; }
 }
